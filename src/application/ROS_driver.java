0ff10b0d3b12d558589ea809d808fc44087cec9d
labsaiadm <labsaiadm@sai-port1.tekniker.es>
2018-05-22 13:23:41 +0200

    --- automatic rebase to remote; old local SHA: c4c7c54fb232383ab4742fdc921a6258b1023972 ---

--- src/application/ROS_driver.java
+++ src/application/ROS_driver.java
@@ -299,6 +299,7 @@ public class ROS_driver extends RoboticsAPIApplication {
 	
 			if (!simulation){
 				//directServo.setJointVelocityRel(jointSpeed.get());
+				directMotion.setMinimumTrajectoryExecutionTime(20e-3);
 				directMotion.setDestination(jointPosition);
 			} else {
 				simulation_joints = jointPosition;
