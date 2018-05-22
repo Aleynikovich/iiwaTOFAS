a4771c75bf54aace0a9c1a2b88b050826b712112
labsaiadm <labsaiadm@sai-port1.tekniker.es>
2018-05-22 12:55:14 +0200

    --- automatic rebase to remote; old local SHA: 45a9f5272ff31586b3e99c37078bcd1f9addcb0d ---

--- src/application/ROS_driver.java
+++ src/application/ROS_driver.java
@@ -226,7 +226,7 @@ public class ROS_driver extends RoboticsAPIApplication {
 		if (lastRobotMode != RobotMode.smart){
 			if (motionContainer != null) motionContainer.cancel();
 			//smartServo.overrideJointAcceleration(10.0);
-			//smartServo.setJointAccelerationRel(1.0);
+			smartServo.setJointAccelerationRel(1.0);
 			smartServo.setJointVelocityRel(1.0);
 			motionContainer = robot.moveAsync(smartServo);
 			smartMotion = smartServo.getRuntime();
