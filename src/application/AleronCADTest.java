bfcca893ae71628829023d04d76cb99ea33368a4
Usuario <Usuario@Mountain>
2018-10-01 15:22:59 +0200

    --- automatic rebase to remote; old local SHA: 7ba8a3cffd9d83b692964fbfd32421d5cf023880 ---

--- src/application/AleronCADTest.java
+++ src/application/AleronCADTest.java
@@ -321,7 +321,7 @@ public class AleronCADTest extends RoboticsAPIApplication {
 
 			point.setRedundancyInformation(lbr, redundancyInfo);
 			
-			point.transform(XyzAbcTransformation.ofDeg(0, 0, 0, -72.67, 0.0, 180));
+			point.transform(XyzAbcTransformation.ofDeg(0, 0, 0, -72.67, 1.27, 180));
 			//new_point.setRedundancyInformation(lbr, redundancyInfo);
 				
 			//System.out.println("x: " + new_point.getX() + " y: " + new_point.getY() + " z: " + new_point.getZ() + 
