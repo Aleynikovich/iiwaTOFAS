72db7b94faa26eef167dd9ea708390a042403cb0
Usuario <Usuario@Mountain>
2018-10-08 15:38:21 +0200

    --- automatic rebase to remote; old local SHA: 004e8c9c86dc8cbe4cb7243cb37bd50ca8037a5e ---

---
+++ src/application/RosUdpDriver.java
@@ -0,0 +1,68 @@
+package application;
+
+
+import javax.inject.Inject;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see UseRoboticsAPIContext
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class RosUdpDriver extends RoboticsAPIApplication {
+	@Inject
+	private LBR lBR_iiwa_14_R820_1;
+	
+	
+	public static void main(String[] args) {
+		RosUdpDriver app = new RosUdpDriver();
+		app.runApplication();
+	}
+
+	@Override
+	public void initialize() {
+		// initialize your application here
+	}
+	
+	
+	@Override
+	public void dispose(){
+		System.out.println("Stoping motion... ");
+		System.out.println("Closing the sockets... ");
+//		try { clientSocket.close(); } catch (Exception e) { }
+//		try { serverSocket.close(); } catch (Exception e) { }
+	}
+
+	@Override
+	public void run() {
+		
+		System.out.println("Initializing tcp server... ");
+		try{
+			
+			while(true)
+			{}
+		} catch (Exception e){
+			// Stop button clicked in the control pad or critical error
+			// Sockets are close in the dispose function
+			// Some events like a broken connection could reach this Exception Handler,
+			// but I considered better to start again the program than don't stop the robot
+			
+		}
+	
+	}
+}
