52243e8a0174da8fbb386ece080897a58de73119
Usuario <Usuario@Mountain>
2018-10-08 12:55:41 +0200

    --- automatic rebase to remote; old local SHA: d702674d947a3cc1ae4640bdc3bb5beb0d680ef4 ---

--- src/application/multi_threads.java
+++ src/application/multi_threads.java
@@ -44,7 +44,7 @@ class EchoServer extends Thread {
  
     public EchoServer() {
         try {
-			socket = new DatagramSocket(30202);
+			socket = new DatagramSocket(30200);
 		} catch (SocketException e) {
             System.out.println(e.toString());
 
