97a390306ac90e921e98f582c4b0f612efdfc62e
Usuario <Usuario@Mountain>
2018-10-08 16:56:22 +0200

    --- automatic rebase to remote; old local SHA: 1c19d373590d52e2c3f92cdc9f1b4ee199eceae9 ---

--- src/application/RosUdpDriver.java
+++ src/application/RosUdpDriver.java
@@ -45,14 +45,7 @@ class EchoServer extends Thread {
     private byte[] buf = new byte[256];
     private volatile boolean flag = true;
      
-    //This method will set flag as false
-     
-    public void stopRunning()
-    {
-        flag = false;
-		System.out.println("stopping running");
 
-    }
  
     public EchoServer() {
         try {
@@ -65,29 +58,29 @@ class EchoServer extends Thread {
     @Override
     public void run() {
 
-        while (flag) {
-    		System.out.println("in thread");
-
-//            DatagramPacket packet 
-//              = new DatagramPacket(buf, buf.length);
-//            try {
-//				socket.receive(packet);
-//			} catch (IOException e) {
-//				System.out.println(e.toString());
-//			}
-             
-//            InetAddress address = packet.getAddress();
-//            int port = packet.getPort();
-//            packet = new DatagramPacket(buf, buf.length, address, port);
-//            String received 
-//              = new String(packet.getData(), 0, packet.getLength());
+//        while (flag) {
+//    		System.out.println("in thread");
+//
+////            DatagramPacket packet 
+////              = new DatagramPacket(buf, buf.length);
+////            try {
+////				socket.receive(packet);
+////			} catch (IOException e) {
+////				System.out.println(e.toString());
+////			}
 //             
-//            if (received.equals("end")) {
-//                flag = false;
-//                continue;
-//            }
-//            socket.send(packet);
-        }
+////            InetAddress address = packet.getAddress();
+////            int port = packet.getPort();
+////            packet = new DatagramPacket(buf, buf.length, address, port);
+////            String received 
+////              = new String(packet.getData(), 0, packet.getLength());
+////             
+////            if (received.equals("end")) {
+////                flag = false;
+////                continue;
+////            }
+////            socket.send(packet);
+//        }
 		System.out.println("Leaving the thread");
 
         socket.close();
