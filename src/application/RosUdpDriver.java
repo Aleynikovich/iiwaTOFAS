ec2575427725abe94b8c098c7b6d366e085f6bd3
Usuario <Usuario@Mountain>
2018-10-08 16:58:58 +0200

    --- automatic rebase to remote; old local SHA: f9eaf977b5d0f4e7d804429c5ca4187f7a918edb ---

--- src/application/RosUdpDriver.java
+++ src/application/RosUdpDriver.java
@@ -58,29 +58,29 @@ class EchoServer extends Thread {
     @Override
     public void run() {
 
-//        while (flag) {
-//    		System.out.println("in thread");
-//
-////            DatagramPacket packet 
-////              = new DatagramPacket(buf, buf.length);
-////            try {
-////				socket.receive(packet);
-////			} catch (IOException e) {
-////				System.out.println(e.toString());
-////			}
+        while (!Thread.interrupted()) {
+    		System.out.println("in thread");
+
+//            DatagramPacket packet 
+//              = new DatagramPacket(buf, buf.length);
+//            try {
+//				socket.receive(packet);
+//			} catch (IOException e) {
+//				System.out.println(e.toString());
+//			}
+             
+//            InetAddress address = packet.getAddress();
+//            int port = packet.getPort();
+//            packet = new DatagramPacket(buf, buf.length, address, port);
+//            String received 
+//              = new String(packet.getData(), 0, packet.getLength());
 //             
-////            InetAddress address = packet.getAddress();
-////            int port = packet.getPort();
-////            packet = new DatagramPacket(buf, buf.length, address, port);
-////            String received 
-////              = new String(packet.getData(), 0, packet.getLength());
-////             
-////            if (received.equals("end")) {
-////                flag = false;
-////                continue;
-////            }
-////            socket.send(packet);
-//        }
+//            if (received.equals("end")) {
+//                flag = false;
+//                continue;
+//            }
+//            socket.send(packet);
+        }
 		System.out.println("Leaving the thread");
 
         socket.close();
