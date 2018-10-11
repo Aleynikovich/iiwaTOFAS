57d78a5a237e9178a1c3b56cde69a56fe9291502
Usuario <Usuario@Mountain>
2018-10-11 15:51:14 +0200

    --- automatic rebase to remote; old local SHA: 4135d77f7bfb94ad5d7d9755c0dfec4816e656c5 ---

--- src/application/RosUdpDriver.java
+++ src/application/RosUdpDriver.java
@@ -188,7 +188,7 @@ class EchoServer extends Thread {
 				//System.out.println(e.toString());
 				received_packet = false;
 			} catch (IOException e) {
-				//System.out.println(e.toString());
+				System.out.println(e.toString());
 				received_packet = false;
 			}
             
@@ -196,7 +196,7 @@ class EchoServer extends Thread {
         		//System.out.println("received");
 
             	String[] commands = parseDatagram(packet);
-            	System.out.println(commands.length);
+            	//System.out.println(commands.length);
             	setRobotCommand(commands);  	
             }
              
