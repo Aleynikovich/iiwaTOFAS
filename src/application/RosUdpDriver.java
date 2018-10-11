c9a22fd82e29d6179dec40bbbe48df2562f58d46
Usuario <Usuario@Mountain>
2018-10-11 09:01:22 +0200

    --- automatic rebase to remote; old local SHA: 7f3e62c7557ec007f5f228be56b3ad570ee2b8e3 ---

--- src/application/RosUdpDriver.java
+++ src/application/RosUdpDriver.java
@@ -50,7 +50,7 @@ class EchoServer extends Thread {
  
     public EchoServer() {
         try {
-			socket = new DatagramSocket(30201);
+			socket = new DatagramSocket(30200);
 		} catch (SocketException e) {
 			System.out.println(e.toString());
 		}
@@ -140,7 +140,7 @@ class EchoClient extends Thread {
     public void sendEcho(String msg) {
         buf = msg.getBytes();
         DatagramPacket packet 
-          = new DatagramPacket(buf, buf.length, address, 30201);
+          = new DatagramPacket(buf, buf.length, address, 30200);
         try {
 			socket.send(packet);
 		} catch (IOException e) {
