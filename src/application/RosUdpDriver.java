31a01718c2a3e2c53d45685345e95437ddae1ff6
Usuario <Usuario@Mountain>
2018-10-16 14:51:00 +0200

    --- automatic rebase to remote; old local SHA: d659f293e3727400107d10ed054b37e2fb5255ec ---

--- src/application/RosUdpDriver.java
+++ src/application/RosUdpDriver.java
@@ -217,7 +217,7 @@ class EchoServer extends Thread {
 		}
         
     	try {
-			writer = new PrintWriter("C:/KRC/ROBOTER/log/DataRecorder/test/test1_16.txt", "UTF-8");
+			writer = new PrintWriter("C:/KRC/ROBOTER/log/DataRecorder/test/test1_18.txt", "UTF-8");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -611,7 +611,7 @@ class SmartControl extends Thread {
 	public SmartControl(){
 		RosUdpDriver.received_packet_bool = false;
     	try {
-			writer = new PrintWriter("C:/KRC/ROBOTER/log/DataRecorder/test/test_Scontrol_16.txt", "UTF-8");
+			writer = new PrintWriter("C:/KRC/ROBOTER/log/DataRecorder/test/test_Scontrol_18.txt", "UTF-8");
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
