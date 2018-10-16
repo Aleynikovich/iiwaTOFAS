1f06fcfd9ebfb41ba37bfca81c51b04f61f1f3de
Usuario <Usuario@Mountain>
2018-10-16 14:48:04 +0200

    --- automatic rebase to remote; old local SHA: 0d012dedcdbb144df27ada2a1d2d714f71372afe ---

--- src/application/RosUdpDriver.java
+++ src/application/RosUdpDriver.java
@@ -660,6 +660,7 @@ class SmartControl extends Thread {
 
 				smartMotion.setMinimumTrajectoryExecutionTime(30e-3);
 
+            long t0 = System.currentTimeMillis();
 			try{
 				smartMotion.setDestination(jointPosition);//, jointSpeed);
 				
@@ -670,6 +671,14 @@ class SmartControl extends Thread {
 				stop_running();
 				
 			}
+            long t1 = System.currentTimeMillis();
+            
+            long dt = t1-t0;
+            if(dt>0.01)
+            {
+            System.out.println(dt);
+            }
+			
 			RosUdpDriver.lastRobotMode = RosUdpDriver.RobotMode.smart;
 	    } catch(Exception e) {
 			//System.out.println(e.toString());
