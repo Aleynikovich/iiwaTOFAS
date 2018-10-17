d2e810e64ee708e9b5ec119683e733b1a7ff4bdf
Usuario <Usuario@Mountain>
2018-10-17 08:55:02 +0200

    --- automatic rebase to remote; old local SHA: a3ce108716095e4f26355f8f48736dc60329d653 ---

--- src/application/RosUdpDriver.java
+++ src/application/RosUdpDriver.java
@@ -266,7 +266,7 @@ class EchoServer extends Thread {
 
 		try {
 			socket.setSoTimeout(1000);
-			socket.setReceiveBufferSize(buf.length);
+			//socket.setReceiveBufferSize(buf.length);
 		} catch (SocketException e1) {
 			e1.printStackTrace();
 		}
