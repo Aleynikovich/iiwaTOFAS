63083f239dc7bdf7f8bc6f4c59d05ea7fb54af32
Usuario <Usuario@Mountain>
2018-10-17 09:28:14 +0200

    --- automatic rebase to remote; old local SHA: 3ad54160a536e4d810b2584db34dec8528f424c8 ---

--- src/application/RosUdpDriver.java
+++ src/application/RosUdpDriver.java
@@ -755,7 +755,7 @@ class SmartControl extends Thread {
 						}
 	            }
 	          try {
-				Thread.sleep(10);
+				Thread.sleep(1);
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
