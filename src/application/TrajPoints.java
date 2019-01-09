ab248542db21f2f74f746f9bb9486f751bfe3bc3
Usuario <Usuario@Mountain>
2019-01-09 16:24:03 +0100

    --- automatic rebase to remote; old local SHA: c011bfc8989d682a537e234f4bc20eb1b26c159e ---

--- src/application/TrajPoints.java
+++ src/application/TrajPoints.java
@@ -65,7 +65,11 @@ public class TrajPoints extends RoboticsAPIApplication{
 		    		
 		    		 val = Double.parseDouble(data[i]);
 		    		 j_pose.add(val);
+				     System.out.println(j_pose.toString());
+
 		    	 } 		 	 
+		    	 
+		    	 
 		    	 joints_poses.add(j_pose);
 		    	
 		     }
