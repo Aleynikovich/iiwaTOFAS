8acf2198fd141e173d2a26075642204f68a1036b
Usuario <Usuario@Mountain>
2019-03-21 16:57:29 +0100

    --- automatic rebase to remote; old local SHA: 6edc0182bbfe7e962775f2c5356ef11f1411b72b ---

--- src/application/TCPServer.java
+++ src/application/TCPServer.java
@@ -44,7 +44,7 @@ public class TCPServer implements Runnable {
 	@Inject
 	public TCPServer() throws IOException
 	{		
-		socket = new ServerSocket(7000);	
+		socket = new ServerSocket(7001);	
 		connectionSocket = null;
 		listeners = new ArrayList<ITCPListener>();
 		tcpServerThread = null;
