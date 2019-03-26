783706f8652420d4af1f6934f376a8b5bdf588a6
Usuario <Usuario@Mountain>
2019-03-26 10:13:24 +0100

    --- automatic rebase to remote; old local SHA: b831fcb8793fae9f4c8672d2d96c2fa08a8a9259 ---

--- src/application/AleronDemo.java
+++ src/application/AleronDemo.java
@@ -456,10 +456,14 @@ public class AleronDemo extends RoboticsAPIApplication implements ITCPListener{
 				" A: " + caltab_robot_fr.getAlphaRad() + " B: " + caltab_robot_fr.getBetaRad() + " C: " + caltab_robot_fr.getGammaRad());
 		
 		
-		Frame copy_caltab_robot_fr  = null; //= new Frame(caltab_robot_fr);
+		Frame copy_caltab_robot_fr = new Frame(caltab_robot_fr);
+		
+		System.out.println("Copy Caltab frame --> x: " + copy_caltab_robot_fr.getX() + " y: " + copy_caltab_robot_fr.getY() + " z: " + copy_caltab_robot_fr.getZ() + 
+				" A: " + copy_caltab_robot_fr.getAlphaRad() + " B: " + copy_caltab_robot_fr.getBetaRad() + " C: " + copy_caltab_robot_fr.getGammaRad());
+	
 		for(int i=0; i<x.size();i++)
 		{
-			//copy_caltab_robot_fr = //new Frame(caltab_robot_fr);
+			copy_caltab_robot_fr = //new Frame(caltab_robot_fr);
 				caltab_robot_fr.copy(copy_caltab_robot_fr);
 			
 			point.setX(x.get(i)); point.setY(y.get(i)); point.setZ(z.get(i));
