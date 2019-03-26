62c3a72701e13340416ded74c77f5c261a3d1832
Usuario <Usuario@Mountain>
2019-03-26 09:45:48 +0100

    --- automatic rebase to remote; old local SHA: d84a3ed8ca32e6671477945cd94dd569ba5df968 ---

--- src/application/AleronDemo.java
+++ src/application/AleronDemo.java
@@ -466,8 +466,9 @@ public class AleronDemo extends RoboticsAPIApplication implements ITCPListener{
 			 	
 			point.setRedundancyInformation(lbr, redundancyInfo);
 			*/
-			
-			
+			System.out.println("Caltab frame --> x: " + copy_caltab_robot_fr.getX() + " y: " + copy_caltab_robot_fr.getY() + " z: " + copy_caltab_robot_fr.getZ() + 
+					" A: " + copy_caltab_robot_fr.getAlphaRad() + " B: " + copy_caltab_robot_fr.getBetaRad() + " C: " + copy_caltab_robot_fr.getGammaRad());
+									
 			System.out.println("x: " + point.getX() + " y: " + point.getY() + " z: " + point.getZ() + 
 					" A: " + point.getAlphaRad() + " B: " + point.getBetaRad() + " C: " + point.getGammaRad());
 			
