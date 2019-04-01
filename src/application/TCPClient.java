feeda71f6d5a4a1970964519aa19ef350316b6a3
Usuario <Usuario@Mountain>
2019-04-01 10:26:42 +0200

    --- automatic rebase to remote; old local SHA: 2668c866b54cec760de889b6f12c0f74fad0609f ---

--- src/application/TCPClient.java
+++ src/application/TCPClient.java
@@ -80,7 +80,7 @@ public class TCPClient implements Runnable {
 		try {
 			
 			start_listening.set(true);
-			datagram="-255.015;-476.083;395.091;3.129;0.0;3.135\n";
+			datagram="255.015;-476.083;395.091;3.129;0.0;3.135\n";
 		
 			outToServer.writeBytes(datagram);
 			System.out.println(datagram);
