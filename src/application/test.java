a9be23af043c3e765edd30b8bf8a5fde6dfd285b
Usuario <Usuario@Mountain>
2019-04-23 16:56:11 +0200

    --- automatic rebase to remote; old local SHA: be429ea5838670b1a4d0834ca541621e49caeb3b ---

--- src/application/test.java
+++ src/application/test.java
@@ -67,7 +67,12 @@ public class test extends RoboticsAPIApplication implements ISignalListener {
 		 }
 		 */
 		
-
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	@Override
