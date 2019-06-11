41983d0f4b9ecffe40286917e1ca203c9ed50c85
Usuario <Usuario@Mountain>
2019-06-11 13:44:44 +0200

    --- automatic rebase to remote; old local SHA: 94c55b0b1e8766a566041b6dafc7c52b5d081dab ---

--- src/application/AleronDemo2.java
+++ src/application/AleronDemo2.java
@@ -164,7 +164,7 @@ public class AleronDemo2 extends RoboticsAPIApplication implements ITCPListener,
 		Frame pose = new Frame(getFrame("/DemoCroinspect/aileron"));
 		
 		//Catlab1 Aileron frame definition
-		pose.setX(-0.206668293869192 * 1000); pose.setY(0.0938811705121523*1000); pose.setZ(0.0231866294311087*1000);
+		pose.setX(-0.206668293869192 * 1000); pose.setY(0.0938811705121523*1000); pose.setZ(0.39*1000);
 		pose.setAlphaRad(273.904622591501*(Math.PI/180)); pose.setBetaRad(350.013193339373); pose.setGammaRad(178.636440604839*(Math.PI/180));
 
 		
