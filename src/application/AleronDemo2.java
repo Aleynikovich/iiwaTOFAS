d2bb15df9bbad1ca378b837779e3002d7c330087
Usuario <Usuario@Mountain>
2019-06-13 16:57:01 +0200

    --- automatic rebase to remote; old local SHA: 1986c004af254dba6e47081bec11628a33c4abd8 ---

--- src/application/AleronDemo2.java
+++ src/application/AleronDemo2.java
@@ -158,9 +158,12 @@ public class AleronDemo2 extends RoboticsAPIApplication implements ITCPListener,
 		tcp_camera_fr = new Frame(lbr.getFlange());
 		//tcp_camera_fr.setX(-20.0); tcp_camera_fr.setY(-101.902); tcp_camera_fr.setZ(105.038);
 		//tcp_camera_fr.setAlphaRad(0.375 *(Math.PI/180)); tcp_camera_fr.setBetaRad(359.535*(Math.PI/180)); tcp_camera_fr.setGammaRad(1.39*(Math.PI/180));
-		tcp_camera_fr.setX(-14.7231); tcp_camera_fr.setY(-102.098); tcp_camera_fr.setZ(106.308);
-		tcp_camera_fr.setAlphaRad(1.3937*(Math.PI/180)); tcp_camera_fr.setBetaRad(3.086*(Math.PI/180)); tcp_camera_fr.setGammaRad(0.316*(Math.PI/180));
+		//tcp_camera_fr.setX(-14.7231); tcp_camera_fr.setY(-102.098); tcp_camera_fr.setZ(106.308);
+		//tcp_camera_fr.setAlphaRad(1.3937*(Math.PI/180)); tcp_camera_fr.setBetaRad(3.086*(Math.PI/180)); tcp_camera_fr.setGammaRad(0.316*(Math.PI/180));
 	
+		tcp_camera_fr.setX(-13.7524); tcp_camera_fr.setY(-103.628); tcp_camera_fr.setZ(96.0968);
+		tcp_camera_fr.setAlphaRad(2.11153*(Math.PI/180)); tcp_camera_fr.setBetaRad(1.30715*(Math.PI/180)); tcp_camera_fr.setGammaRad(0.784201*(Math.PI/180));
+
 		Frame pose = new Frame(getFrame("/DemoCroinspect/aileron"));
 		
 		//Catlab1 Aileron frame definition
