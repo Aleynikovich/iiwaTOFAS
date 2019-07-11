f11a81c87473919314451bff3e7b9e673af43a23
Usuario <Usuario@Mountain>
2019-07-11 13:21:23 +0200

    --- automatic rebase to remote; old local SHA: c36ac2cd69deb4d12ac66590c0bc7994e26aa6c1 ---

--- src/application/AleronDemo2.java
+++ src/application/AleronDemo2.java
@@ -789,7 +789,7 @@ public class AleronDemo2 extends RoboticsAPIApplication implements ITCPListener,
 				{
 					//System.out.println("Warning signal: " + warning_signal.get());
 					//IMotionContainer motion_cmd = roll_scan.getFrame("roll_tcp").moveAsync(lin(copy_caltab_robot_fr).setCartVelocity(velocidad).setMode(impedanceControlMode).setBlendingCart(10));
-					IMotionContainer motion_cmd = roll_scan.getFrame("roll_tcp").move(lin(copy_caltab_robot_fr).setCartVelocity(velocidad).setMode(impedanceControlMode).setBlendingCart(0));
+					IMotionContainer motion_cmd = roll_scan.getFrame("roll_tcp").move(lin(copy_caltab_robot_fr).setCartVelocity(velocidad));//.setMode(impedanceControlMode).setBlendingCart(0));
 					motion_list.add(motion_cmd);
 					System.out.println(i + " Traj point in robot frame --> x: " + copy_caltab_robot_fr.getX() + " y: " + copy_caltab_robot_fr.getY() + " z: " + copy_caltab_robot_fr.getZ() + 
 							" A: " + copy_caltab_robot_fr.getAlphaRad()*(180/Math.PI) + " B: " + copy_caltab_robot_fr.getBetaRad()*(180/Math.PI) + " C: " + copy_caltab_robot_fr.getGammaRad()*(180/Math.PI) );
