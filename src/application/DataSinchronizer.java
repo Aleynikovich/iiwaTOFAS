3fa3f2ec49adde1c633d89df0f3a255368706e5e
Usuario <Usuario@Mountain>
2019-07-12 12:32:19 +0200

    --- automatic rebase to remote; old local SHA: 3fda6357125694e178cf5b1fbc29c2be8a1465ee ---

--- src/application/DataSinchronizer.java
+++ src/application/DataSinchronizer.java
@@ -39,7 +39,7 @@ public class DataSinchronizer extends RoboticsAPICyclicBackgroundTask implements
 	public void initialize() {
 		// initialize your task here
 		initializeCyclic(0, 1000, TimeUnit.MILLISECONDS,CycleBehavior.BestEffort);
-		sinc = true;
+		sinc = false;
 	}
 
 	@Override
