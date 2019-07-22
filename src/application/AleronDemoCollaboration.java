cb261bc130dee0362e975754c2ec6c57e92a17b3
Usuario <Usuario@Mountain>
2019-07-22 12:30:19 +0200

    --- automatic rebase to remote; old local SHA: 1b202c763a6fa4dce4fec659a045e6dc8703ab2c ---

--- src/application/AleronDemoCollaboration.java
+++ src/application/AleronDemoCollaboration.java
@@ -133,12 +133,12 @@ public class AleronDemoCollaboration extends RoboticsAPIApplication implements I
 		// Init springs
 		gravityCompensatioMode = new CartesianImpedanceControlMode();
 		gravityCompensatioMode.setMaxCartesianVelocity(200.0,200.0,200.0,Math.toRadians(60),Math.toRadians(60),Math.toRadians(60));
-		gravityCompensatioMode.parametrize(CartDOF.X).setStiffness(50).setDamping(1.0);
-		gravityCompensatioMode.parametrize(CartDOF.Y).setStiffness(50).setDamping(1.0);
-		gravityCompensatioMode.parametrize(CartDOF.Z).setStiffness(50).setDamping(1.0);
-		gravityCompensatioMode.parametrize(CartDOF.A).setStiffness(50).setDamping(1.0);
-		gravityCompensatioMode.parametrize(CartDOF.B).setStiffness(50).setDamping(1.0);
-		gravityCompensatioMode.parametrize(CartDOF.C).setStiffness(10).setDamping(1.0);	
+		gravityCompensatioMode.parametrize(CartDOF.X).setStiffness(5).setDamping(1.0);
+		gravityCompensatioMode.parametrize(CartDOF.Y).setStiffness(5).setDamping(1.0);
+		gravityCompensatioMode.parametrize(CartDOF.Z).setStiffness(5).setDamping(1.0);
+		gravityCompensatioMode.parametrize(CartDOF.A).setStiffness(5).setDamping(1.0);
+		gravityCompensatioMode.parametrize(CartDOF.B).setStiffness(5).setDamping(1.0);
+		gravityCompensatioMode.parametrize(CartDOF.C).setStiffness(1).setDamping(1.0);	
 
 				
 	} 
