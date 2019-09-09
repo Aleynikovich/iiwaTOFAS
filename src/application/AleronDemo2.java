3e89bf9229517a7573fa574713bb004e514c09c3
Usuario <Usuario@Mountain>
2019-09-09 09:41:18 +0200

    --- automatic rebase to remote; old local SHA: 3ae72594ec3d0a2775f0253dd299105144ddaa2d ---

--- src/application/AleronDemo2.java
+++ src/application/AleronDemo2.java
@@ -446,7 +446,7 @@ public class AleronDemo2 extends RoboticsAPIApplication implements ITCPListener,
 		{
 			if(traj_caltab_ref_fr.get(i).getX()== traj_caltab_ref_fr.get(i-1).getX())
 			{	
-				traj_caltab_ref_fr.get(i).transform(XyzAbcTransformation.ofDeg(0.0, 0.0, -10.0, 0.0, 0.0, 0.0));
+				traj_caltab_ref_fr.get(i).transform(XyzAbcTransformation.ofDeg(0.0, 0.0, -75.0, 0.0, 0.0, 0.0));
 				
 				/*System.out.println(i + " Traj Position in caltab frame --> x: " + traj_caltab_ref_fr.get(i).getX() + 
 					" y: " + traj_caltab_ref_fr.get(i).getY() + " z: " + traj_caltab_ref_fr.get(i).getZ() + 
@@ -455,7 +455,7 @@ public class AleronDemo2 extends RoboticsAPIApplication implements ITCPListener,
 				*/
 				i++;
 				
-				traj_caltab_ref_fr.get(i).transform(XyzAbcTransformation.ofDeg(0.0, 0.0, -10.0, 0.0, 0.0, 0.0));
+				traj_caltab_ref_fr.get(i).transform(XyzAbcTransformation.ofDeg(0.0, 0.0, -75.0, 0.0, 0.0, 0.0));
 				
 				/*System.out.println(i + " Traj Position in caltab frame --> x: " + traj_caltab_ref_fr.get(i).getX() + 
 						" y: " + traj_caltab_ref_fr.get(i).getY() + " z: " + traj_caltab_ref_fr.get(i).getZ() + 
@@ -938,8 +938,8 @@ public class AleronDemo2 extends RoboticsAPIApplication implements ITCPListener,
 					//Comprobar si en el proximo punto tiene contacto o no con la superficie
 					Frame contactless_point = traj_caltab_ref_fr.get(i+1).copy();
 					Frame contact_point = traj_caltab_ref_fr.get(i-1).copy();
-					contactless_point.transform(XyzAbcTransformation.ofRad(0.0, 0.0,10.0,0.0,0.0,0.0));
-					contact_point.transform(XyzAbcTransformation.ofRad(0.0, 0.0,10.0,0.0,0.0,0.0));
+					contactless_point.transform(XyzAbcTransformation.ofRad(0.0, 0.0,75.0,0.0,0.0,0.0));
+					contact_point.transform(XyzAbcTransformation.ofRad(0.0, 0.0,75.0,0.0,0.0,0.0));
 					
 					/*System.out.println("Traj point in caltab frame --> x: " + point.getX() + " y: " + point.getY() + " z: " + point.getZ() + 
 							" A: " + point.getAlphaRad()*(180/Math.PI) + " B: " + point.getBetaRad()*(180/Math.PI) + " C: " + point.getGammaRad()*(180/Math.PI) );
@@ -986,7 +986,7 @@ public class AleronDemo2 extends RoboticsAPIApplication implements ITCPListener,
 								" A: " + copy_caltab_robot_fr.getAlphaRad()*(180/Math.PI) + " B: " + copy_caltab_robot_fr.getBetaRad()*(180/Math.PI) + " C: " + copy_caltab_robot_fr.getGammaRad()*(180/Math.PI) );
 				
 						//IMotionContainer motion_cmd = roll_scan.getFrame("Gripper").move(lin(copy_caltab_robot_fr).setCartVelocity(10).setMode(impedanceControlMode).setBlendingCart(0));
-						IMotionContainer motion_cmd = tool_frame.move(lin(copy_caltab_robot_fr).setCartVelocity(1).setMode(impedanceControlMode).setBlendingCart(0.0));
+						IMotionContainer motion_cmd = tool_frame.move(lin(copy_caltab_robot_fr).setCartVelocity(10).setMode(impedanceControlMode).setBlendingCart(0.0));
 						motion_list.add(motion_cmd);
 												
 						IFiredConditionInfo firedInfo =  motion_cmd.getFiredBreakConditionInfo();
@@ -1010,7 +1010,7 @@ public class AleronDemo2 extends RoboticsAPIApplication implements ITCPListener,
 						//Movimiento de flanco de subida o de bajada de la almena
 						//System.out.println("Warning signal: " + warning_signal.get());
 						IMotionContainer motion_cmd; 
-						if(impedance_off)
+						/*if(impedance_off)
 						{
 							System.out.println("Movement without impedance");
 							//motion_cmd = roll_scan.getFrame("Gripper").moveAsync(lin(copy_caltab_robot_fr).setCartVelocity(velocidad).setBlendingCart(10));
@@ -1023,10 +1023,10 @@ public class AleronDemo2 extends RoboticsAPIApplication implements ITCPListener,
 							}
 						}
 						else
-						{
+						{*/
 							//motion_cmd = roll_scan.getFrame("Gripper").moveAsync(lin(copy_caltab_robot_fr).setCartVelocity(velocidad).setMode(impedanceControlMode).setBlendingCart(10));
-							motion_cmd = tool_frame.moveAsync(lin(copy_caltab_robot_fr).setCartVelocity(velocidad).setMode(impedanceControlMode).setBlendingCart(10));
-						}
+						motion_cmd = tool_frame.moveAsync(lin(copy_caltab_robot_fr).setCartVelocity(velocidad).setMode(impedanceControlMode).setBlendingCart(10));
+						//}
 						
 						motion_list.add(motion_cmd);
 						System.out.println(i + " Traj point in robot frame --> x: " + copy_caltab_robot_fr.getX() + " y: " + copy_caltab_robot_fr.getY() + " z: " + copy_caltab_robot_fr.getZ() + 
