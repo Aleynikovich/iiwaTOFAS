efb29dac18b653d49c2629851f659189831f7483
Usuario <Usuario@Mountain>
2020-05-13 12:38:28 +0200

    --- automatic rebase to remote; old local SHA: 20158681c18cc80200b736f5e4dd239aaf9f29f8 ---

--- src/application/BinPicking_EKI.java
+++ src/application/BinPicking_EKI.java
@@ -1,10 +1,20 @@
 package application;
 
 
+import java.io.IOException;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import javax.inject.Inject;
+
+import com.kuka.generated.ioAccess.MediaFlangeIOGroup;
 import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
 import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+
+import com.kuka.roboticsAPI.controllerModel.Controller;
 import com.kuka.roboticsAPI.deviceModel.LBR;
+import com.kuka.roboticsAPI.geometricModel.Frame;
+import com.kuka.roboticsAPI.geometricModel.Tool;
+import com.kuka.roboticsAPI.motionModel.controlModeModel.CartesianImpedanceControlMode;
 
 /**
  * Implementation of a robot application.
@@ -25,18 +35,74 @@ import com.kuka.roboticsAPI.deviceModel.LBR;
  * @see #dispose()
  */
 public class BinPicking_EKI extends RoboticsAPIApplication implements BinPicking_ITCPListener{
+	
 	@Inject
-	private LBR lBR_iiwa_14_R820_1;
+	private LBR lbr;
+    private Controller controller;
+    private Tool binpick;
+    
+    private Frame up_fr;
+	private Frame down_fr;
+	private Frame exit_fr;
+	private double[] gripper_tool_xyz = new double[]{0,0,0.26448};
+	private double[] gripper_tool_rpy = new double[]{0.0,0,-Math.PI/2};
+	
+	
+	
+    boolean exit;
+    
+    String fname;
+    
+    CartesianImpedanceControlMode impedanceControlMode;
+    
+   	private static final int stiffnessZ = 300;
+   	private static final int stiffnessY = 5000;
+   	private static final int stiffnessX = 5000;
+   	
 
+	private BinPicking_TCPServer tcp_server;
+	AtomicBoolean data_received;
+	
+	//Exchanged data info 
+	String operation_type;
+	String time_stamp;
+	Frame caltab_robot_fr;
+   	
+    @Inject
+	private MediaFlangeIOGroup mediaFIO;
+    
 	@Override
 	public void initialize() {
 		// initialize your application here
+		controller = getController("KUKA_Sunrise_Cabinet_1");
+
+		// initialize your application here
+		binpick = createFromTemplate("BinPick_Tool");
+		binpick.attachTo(lbr.getFlange());
+		
+		System.out.println("Roll scan frame: " + binpick.getFrame("tcp").toString());
+		
+		data_received = new AtomicBoolean(false);
+		
+		MediaFlangeIOGroup  FlangeIO= new  MediaFlangeIOGroup(controller);
+		
+		try {
+			tcp_server = new BinPicking_TCPServer();				
+			tcp_server.addListener(this);
+			tcp_server.enable();
+					
+		} catch (IOException e) {
+			//TODO Bloque catch generado automáticamente
+			System.err.println("Could not create TCPServer:" +e.getMessage());
+		}
+		
+		mediaFIO.setLEDBlue(true);
 	}
 
 	@Override
 	public void run() {
 		// your application execution starts here
-		lBR_iiwa_14_R820_1.move(ptpHome());
+		lbr.move(ptpHome());
 	}
 
 	@Override
