4bdb993638ddd9b334ee7237ef6b479f2431d60e
Usuario <Usuario@Mountain>
2020-05-13 12:17:40 +0200

    --- automatic rebase to remote; old local SHA: cc9bdcc0b6f1c7c897773f151ccdf6e78f66bc88 ---

---
+++ src/application/BinPicking_EKI.java
@@ -0,0 +1,53 @@
+package application;
+
+
+import javax.inject.Inject;
+import com.kuka.roboticsAPI.applicationModel.RoboticsAPIApplication;
+import static com.kuka.roboticsAPI.motionModel.BasicMotions.*;
+import com.kuka.roboticsAPI.deviceModel.LBR;
+
+/**
+ * Implementation of a robot application.
+ * <p>
+ * The application provides a {@link RoboticsAPITask#initialize()} and a 
+ * {@link RoboticsAPITask#run()} method, which will be called successively in 
+ * the application lifecycle. The application will terminate automatically after 
+ * the {@link RoboticsAPITask#run()} method has finished or after stopping the 
+ * task. The {@link RoboticsAPITask#dispose()} method will be called, even if an 
+ * exception is thrown during initialization or run. 
+ * <p>
+ * <b>It is imperative to call <code>super.dispose()</code> when overriding the 
+ * {@link RoboticsAPITask#dispose()} method.</b> 
+ * 
+ * @see UseRoboticsAPIContext
+ * @see #initialize()
+ * @see #run()
+ * @see #dispose()
+ */
+public class BinPicking_EKI extends RoboticsAPIApplication implements BinPicking_ITCPListener{
+	@Inject
+	private LBR lBR_iiwa_14_R820_1;
+
+	@Override
+	public void initialize() {
+		// initialize your application here
+	}
+
+	@Override
+	public void run() {
+		// your application execution starts here
+		lBR_iiwa_14_R820_1.move(ptpHome());
+	}
+
+	@Override
+	public void OnTCPMessageReceived(String datagram) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void OnTCPConnection() {
+		// TODO Auto-generated method stub
+		
+	}
+}
