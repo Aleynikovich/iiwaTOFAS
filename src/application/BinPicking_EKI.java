7e7061c67b73f0120f5c449471efc457f475b2ec
sailab <sailab@sailab2.tekniker.es>
2021-05-17 15:55:04 +0200

    --- automatic rebase to remote; old local SHA: 9aa00424570e56f0d56cf18b773c2b03309ba353 ---

--- src/application/BinPicking_EKI.java
+++ src/application/BinPicking_EKI.java
@@ -235,7 +235,7 @@ public class BinPicking_EKI extends RoboticsAPIApplication implements BinPicking
 				
 				/* EL STRING DE LA POSE JUNTO*/
 				  request_str = robot_pose.getX()/1000 + ";" + robot_pose.getY()/1000 + ";" + robot_pose.getZ()/1000 + ";" +
-					robot_pose.getGammaRad() + ";" + robot_pose.getBetaRad()+ ";" + robot_pose.getAlphaRad() + ";" + "5";
+					robot_pose.getGammaRad()*Math.PI/180 + ";" + robot_pose.getBetaRad()*Math.PI/180+ ";" + robot_pose.getAlphaRad()*Math.PI/180 + ";" + "5";
 			
 				System.out.println(frame_name + " -->  " + request_str);
 				System.out.println("data_recived=false");
@@ -276,7 +276,7 @@ public class BinPicking_EKI extends RoboticsAPIApplication implements BinPicking
 			robot_pose = lbr.getCurrentCartesianPosition(lbr.getFlange());
 			
 			request_str = robot_pose.getX()/1000 + ";" + robot_pose.getY()/1000 + ";" + robot_pose.getZ()/1000 + ";" +
-						robot_pose.getGammaRad() + ";" + robot_pose.getBetaRad()+ ";" + robot_pose.getAlphaRad() + ";" + "5" ;
+						robot_pose.getGammaRad()*Math.PI/180 + ";" + robot_pose.getBetaRad()*Math.PI/180+ ";" + robot_pose.getAlphaRad()*Math.PI/180 + ";" + "5" ;
 				
 			System.out.println(request_str);
 		  	System.out.println("data_recived=false");
