2c0cfacd89370f12c2da14dd3b8cac73bd13cc9c
TeknikerTOFAS <TeknikerTOFAS@192.168.46.128>
2024-11-21 10:25:05 +0100

    --- automatic rebase to remote; old local SHA: 6bc2add35c202a59b23c0032eb60dd9994476ea6 ---

--- src/hartuTofas/AARobotTCPServer.java
+++ src/hartuTofas/AARobotTCPServer.java
@@ -3,7 +3,7 @@ package hartuTofas;
 import java.io.*;
 import java.net.*;
 
-public class AAARobotTCPServer {
+public class AARobotTCPServer {
     public static void main(String[] args) {
         int port = 30001; // Listening port
         ServerSocket serverSocket = null;
