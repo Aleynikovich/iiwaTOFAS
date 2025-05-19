cfb0e36abd3b8c63fd3fb11d9a70901931e2cabd
TeknikerTOFAS <TeknikerTOFAS@192.168.46.128>
2025-05-19 08:50:25 +0200

    --- automatic rebase to remote; old local SHA: b62659ff1bcf1fd287bc86bf019c38c3830ff998 ---

--- src/hartuTofas/MessageHandler.java
+++ src/hartuTofas/MessageHandler.java
@@ -185,10 +185,10 @@ public class MessageHandler {
 
                 if (cmd.actionType == PTP_AXIS) {
                     // Execute synchronous PTP motion
-                    robot.move(ptp(jointValues).setJointVelocityRel(0.25));
+                    robot.move(ptp(jointValues).setJointVelocityRel(cmd.speedOverride/10));
                 } else if (cmd.actionType == PTP_AXIS_C) {
                     // Execute asynchronous PTP motion for each point
-                    robot.moveAsync(ptp(jointValues).setJointVelocityRel(0.25).setBlendingRel(0.5));
+                    robot.moveAsync(ptp(jointValues).setJointVelocityRel(cmd.speedOverride/10).setBlendingRel(0.5));
                 }
             }
         } catch (NumberFormatException e) {
