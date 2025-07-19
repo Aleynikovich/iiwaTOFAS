6761ec30e7d8cc820539e12e9c7d12a8cc333dc0
Alexander Kalis <alex.kalis@outlook.com>
2025-07-19 21:27:06 +0200

    --- automatic rebase to remote; old local SHA: 979e12956ce245aa116bc0e133345e323fc8c9a0 ---

--- src/hartu/robot/communication/server/ServerPortListener.java
+++ src/hartu/robot/communication/server/ServerPortListener.java
@@ -73,7 +73,7 @@ public class ServerPortListener implements Runnable
                 // Send FREE|0# if this is the Task Listener
                 if ("Task Listener".equals(listenerName)) {
                     handler.sendMessage("FREE|0#");
-                    Logger.getInstance().log("Sent 'FREE|0#' to new task client " + clientSocket.getInetAddress().getHostAddress()+"\n");
+                    Logger.getInstance().log("Sent 'FREE|0#' to new task client " + clientSocket.getInetAddress().getHostAddress());
                 }
             }
         }
