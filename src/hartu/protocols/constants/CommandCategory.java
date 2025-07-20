44cc30a2995583d0a33a7684c98aa3cfdf350e1e
Alexander Kalis <alex.kalis@outlook.com>
2025-07-21 00:18:53 +0200

    Revert "--- automatic rebase to remote; old local SHA: 1080436d4607e247e71299f9aee6e18adbbe0a9f ---"
    This reverts commit 0f58d688ff43edea7b22768f1ce7cba88b5852eb, reversing
    changes made to 95cdcd87625db41e73f1657792c2016d9e8355a4.

--- src/hartu/protocols/constants/CommandCategory.java
+++ src/hartu/protocols/constants/CommandCategory.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package hartu.protocols.constants;
 
 /**
@@ -11,17 +10,3 @@ public enum CommandCategory {
     PROGRAM_CALL,
     UNKNOWN
 }
-=======
-package hartu.protocols.constants;
-
-/**
- * Defines the high-level categories for commands.
- * This helps in organizing command processing logic.
- */
-public enum CommandCategory {
-    MOVEMENT,
-    IO,
-    PROGRAM_CALL,
-    UNKNOWN
-}
->>>>>>> 306d3b7241344a82c243fc9830ca5814d11c5fb6
