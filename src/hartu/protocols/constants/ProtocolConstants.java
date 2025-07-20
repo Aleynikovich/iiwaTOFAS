f5894bb18a47d86036eedb00d7931603d63b19c6
aleynikovich <alex.kalis@outlook.com>
2025-07-20 16:52:52 +0200

    Added advanced client session managing.

---
+++ src/hartu/protocols/constants/ProtocolConstants.java
@@ -0,0 +1,64 @@
+package hartu.protocols.constants;
+
+public class ProtocolConstants
+{
+
+    public static final String INITIAL_TASK_CLIENT_RESPONSE = "FREE|0#";
+
+    public static final String MESSAGE_TERMINATOR = "#";
+
+    public static final String PRIMARY_DELIMITER = "\\|";
+
+    public static final String SECONDARY_DELIMITER = ";";
+
+    public static final String MULTI_POINT_DELIMITER = ",";
+
+    public enum ListenerType
+    { // Renamed from ClientType
+        TASK_LISTENER("Task Listener", 0),
+        LOG_LISTENER("Log Listener", 1);
+
+        private final String name;
+        private final int value;
+
+        ListenerType(String name, int value)
+        {
+            this.name = name;
+            this.value = value;
+        }
+
+        public static ListenerType fromString(String listenerTypeName)
+        { // Method name updated
+            for (ListenerType type : ListenerType.values())
+            {
+                if (type.name.equals(listenerTypeName))
+                {
+                    return type;
+                }
+            }
+            throw new IllegalArgumentException("Unknown listener type name: " + listenerTypeName);
+        }
+
+        public static ListenerType fromValue(int listenerTypeValue)
+        { // Method name updated
+            for (ListenerType type : ListenerType.values())
+            {
+                if (type.value == listenerTypeValue)
+                {
+                    return type;
+                }
+            }
+            throw new IllegalArgumentException("Unknown listener type value: " + listenerTypeValue);
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public int getValue()
+        {
+            return value;
+        }
+    }
+}
--- src/hartu/robot/communication/server/ServerPortListener.java
+++ src/hartu/robot/communication/server/ServerPortListener.java
@@ -1,5 +1,8 @@
 package hartu.robot.communication.server;
 
+import hartu.protocols.constants.ProtocolConstants;
+import hartu.protocols.constants.ProtocolConstants.ListenerType;
+
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -8,14 +11,15 @@ import java.util.concurrent.TimeUnit;
 public class ServerPortListener implements Runnable
 {
     private final ServerSocket serverSocket;
-    private final String listenerName;
+    private final ListenerType listenerType;
     private final IClientHandlerCallback clientHandlerCallback;
     private final ServerClass serverInstance;
+    private volatile boolean isRunning = true;
 
-    public ServerPortListener(ServerSocket serverSocket, String listenerName, IClientHandlerCallback callback, ServerClass serverInstance)
+    public ServerPortListener(ServerSocket serverSocket, ListenerType listenerType, IClientHandlerCallback callback, ServerClass serverInstance)
     {
         this.serverSocket = serverSocket;
-        this.listenerName = listenerName;
+        this.listenerType = listenerType;
         this.clientHandlerCallback = callback;
         this.serverInstance = serverInstance;
     }
@@ -25,63 +29,123 @@ public class ServerPortListener implements Runnable
         return serverSocket;
     }
 
+    public void stopListening()
+    {
+        isRunning = false;
+        try
+        {
+            if (!serverSocket.isClosed())
+            {
+                serverSocket.close();
+            }
+        }
+        catch (IOException e)
+        {
+            Logger.getInstance().log(listenerType.getName() + ": Error closing server socket during shutdown: " + e.getMessage());
+        }
+    }
+
     @Override
     public void run()
     {
-        Logger.getInstance().log(listenerName + " started listening on port " + serverSocket.getLocalPort());
+        Logger.getInstance().log(listenerType.getName() + " started listening on port " + serverSocket.getLocalPort());
 
         try (ServerSocket ss = this.serverSocket)
         {
-            do
+            while (isRunning)
             {
-                Logger.getInstance().log(listenerName + ": Waiting for a new client to connect...");
-                Socket clientSocket = ss.accept();
+                Logger.getInstance().log(listenerType.getName() + ": Waiting for a new client to connect...");
+                Socket clientSocket;
+                try
+                {
+                    clientSocket = ss.accept();
+                }
+                catch (IOException e)
+                {
+                    if (!isRunning)
+                    {
+                        Logger.getInstance().log(listenerType.getName() + ": Server socket closed, listener shutting down gracefully.");
+                        break;
+                    }
+                    else
+                    {
+                        Logger.getInstance().log(listenerType.getName() + ": Listener I/O error (unexpected): " + e.getMessage());
+                        continue;
+                    }
+                }
 
-                if ("Task Listener".equals(listenerName))
+                if (listenerType == ListenerType.TASK_LISTENER)
                 {
                     if (!serverInstance.isLogClientConnected())
                     {
-                        Logger.getInstance().log(listenerName + ": Task client connection from " + clientSocket.getInetAddress().getHostAddress() + " rejected. Log client not connected.");
+                        Logger.getInstance().log(listenerType.getName() + ": Task client connection from " + clientSocket.getInetAddress().getHostAddress() + " rejected. Log client not connected.");
                         try
                         {
                             clientSocket.close();
-                        } catch (IOException e)
+                        }
+                        catch (IOException e)
                         {
-                            Logger.getInstance().log(listenerName + ": Error closing rejected task client socket: " + e.getMessage());
+                            Logger.getInstance().log(listenerType.getName() + ": Error closing rejected task client socket: " + e.getMessage());
                         }
                         try
                         {
                             TimeUnit.MILLISECONDS.sleep(1000);
-                        } catch (InterruptedException ie)
+                        }
+                        catch (InterruptedException ie)
                         {
                             Thread.currentThread().interrupt();
-                            Logger.getInstance().log(listenerName + ": Listener interrupted during sleep.");
+                            Logger.getInstance().log(listenerType.getName() + ": Listener interrupted during sleep.");
                         }
                         continue;
                     }
                 }
 
-                Logger.getInstance().log(listenerName + ": Client connected from: " + clientSocket.getInetAddress().getHostAddress());
+                String clientIp = clientSocket.getInetAddress().getHostAddress();
+                String clientName = serverInstance.clientIpToNameMap.get(clientIp);
 
-                ClientHandler handler = new ClientHandler(clientSocket, listenerName);
+                if (clientName == null)
+                {
+                    clientName = "Client-" + serverInstance.clientNameCounter.incrementAndGet();
+                    serverInstance.clientIpToNameMap.put(clientIp, clientName);
+                }
+
+                Logger.getInstance().log(listenerType.getName() + ": Client " + clientName + " (" + clientIp + ") connected.");
+
+                ClientSession session = new ClientSession(clientSocket, listenerType, clientName);
+                ClientHandler handler = new ClientHandler(session);
                 Thread handlerThread = new Thread(handler);
                 handlerThread.setDaemon(true);
                 handlerThread.start();
 
                 if (clientHandlerCallback != null)
                 {
-                    clientHandlerCallback.onClientConnected(handler, listenerName);
+                    clientHandlerCallback.onClientConnected(handler, listenerType);
                 }
 
-                if ("Task Listener".equals(listenerName))
+                if (listenerType == ListenerType.TASK_LISTENER)
                 {
-                    handler.sendMessage("FREE|0#");
-                    Logger.getInstance().log("Sent 'FREE|0#' to new task client " + clientSocket.getInetAddress().getHostAddress());
+                    handler.sendMessage(ProtocolConstants.INITIAL_TASK_CLIENT_RESPONSE);
+                    Logger.getInstance().log("Sent '" + ProtocolConstants.INITIAL_TASK_CLIENT_RESPONSE + "' to new task client " + clientName + " (" + clientIp + ")");
                 }
-            } while (true);
-        } catch (IOException e)
+            }
+        }
+        catch (IOException e)
         {
-            Logger.getInstance().log(listenerName + ": Listener I/O error (server shutting down or port issue): " + e.getMessage());
+            Logger.getInstance().log(listenerType.getName() + ": Listener I/O error (initialization or unexpected): " + e.getMessage());
+        }
+        finally
+        {
+            if (!serverSocket.isClosed())
+            {
+                try
+                {
+                    serverSocket.close();
+                }
+                catch (IOException e)
+                {
+                    Logger.getInstance().log(listenerType.getName() + ": Error closing server socket in finally block: " + e.getMessage());
+                }
+            }
         }
     }
 }
