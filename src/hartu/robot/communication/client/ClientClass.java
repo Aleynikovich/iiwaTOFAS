b12b3548273389a5be07a6c266ee7c59437bf101
aleynikovich <alex.kalis@outlook.com>
2025-07-20 20:34:57 +0200

    Added joint broadcaster client. Added property files for easy network config.

--- src/hartu/robot/communication/client/ClientClass.java
+++ src/hartu/robot/communication/client/ClientClass.java
@@ -1,43 +1,52 @@
 package hartu.robot.communication.client;
 
 import hartu.protocols.constants.ProtocolConstants;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class ClientClass {
+public class ClientClass
+{
     private Socket clientSocket;
     private PrintWriter out;
     private BufferedReader in;
-    private String serverIp;
-    private int serverPort;
+    private final String serverIp;
+    private final int serverPort;
 
-    public ClientClass(String serverIp, int serverPort) {
+    public ClientClass(String serverIp, int serverPort)
+    {
         this.serverIp = serverIp;
         this.serverPort = serverPort;
     }
 
-    public void connect() throws IOException {
+    public void connect() throws IOException
+    {
         clientSocket = new Socket(serverIp, serverPort);
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
     }
 
-    public void sendMessage(String message) {
-        if (out != null) {
+    public void sendMessage(String message)
+    {
+        if (out != null)
+        {
             out.print(message + ProtocolConstants.MESSAGE_TERMINATOR);
             out.flush();
         }
     }
 
-    public String readMessage() throws IOException {
+    public String readMessage() throws IOException
+    {
         StringBuilder messageBuilder = new StringBuilder();
         int charCode;
-        while ((charCode = in.read()) != -1) {
+        while ((charCode = in.read()) != -1)
+        {
             char c = (char) charCode;
-            if (c == ProtocolConstants.MESSAGE_TERMINATOR.charAt(0)) {
+            if (c == ProtocolConstants.MESSAGE_TERMINATOR.charAt(0))
+            {
                 break;
             }
             messageBuilder.append(c);
@@ -45,19 +54,34 @@ public class ClientClass {
         return messageBuilder.toString();
     }
 
-    public void close() throws IOException {
-        if (in != null) {
+    public void close() throws IOException
+    {
+        if (in != null)
+        {
             in.close();
         }
-        if (out != null) {
+        if (out != null)
+        {
             out.close();
         }
-        if (clientSocket != null && !clientSocket.isClosed()) {
+        if (clientSocket != null && !clientSocket.isClosed())
+        {
             clientSocket.close();
         }
     }
 
-    public boolean isConnected() {
+    public boolean isConnected()
+    {
         return clientSocket != null && clientSocket.isConnected() && !clientSocket.isClosed();
+    }
+
+    public String getServerIp()
+    {
+        return serverIp;
+    }
+
+    public int getServerPort()
+    {
+        return serverPort;
     }
 }
