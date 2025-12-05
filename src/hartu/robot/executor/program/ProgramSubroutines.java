2bdb57618a9a810d49a9496ff7bcbcd8744a84be
akalis <alex.kalis@outlook.com>
2025-12-05 13:33:42 +0100

    Add security feature to avoid collisions while tool changing from 1-2->3 and viceversa

--- src/hartu/robot/executor/program/ProgramSubroutines.java
+++ src/hartu/robot/executor/program/ProgramSubroutines.java
@@ -195,13 +195,22 @@ public class ProgramSubroutines
             Logger.getInstance().error("ROBOT_EXEC", "GimaticCamera tool not loaded. Cannot execute place operation.");
             return false;
         }
-        if (toolId > 3)
+        int baseId = toolId;
+        if (baseId > 3)
         {
-            toolId = toolId - 3;
+            baseId = baseId - 3;
         }
-        String baseName = "T" + toolId + "Base";
+        String baseName = "T" + baseId + "Base";
         try
         {
+            //Move to safe pos if we are on right side of plane to avoid collisions
+            if ((robot.getFlange().getY() < 0 && baseId == 3) || (robot.getFlange().getY() > 0 && baseId != 3))
+            {
+                Logger.getInstance().debug("ROBOT_EXEC", "Moving to safe position to avoid collision with tool " + toolId);
+                robot.move(ptp(robot.getCurrentJointPosition().get(0),0,0,0,0,0,0));
+                robot.move(ptpHome());
+            }
+
             ObjectFrame baseFrame = application.getApplicationData().getFrame("/" + baseName);
             if (baseFrame == null)
             {
