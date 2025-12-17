991e44799f6248f4793d5e74cf109b5c3eabc76d
Alexander Kalis <alex.kalis@outlook.com>
2025-12-17 10:58:37 +0100

    --- automatic rebase to remote; old local SHA: 1f5aa63566dfc3b11486407da660d55c5dc34124 ---

--- src/hartu/robot/communication/server/JointStateServerManager.java
+++ src/hartu/robot/communication/server/JointStateServerManager.java
@@ -33,7 +33,7 @@ public class JointStateServerManager extends RoboticsAPICyclicBackgroundTask
     @Override
     public void initialize()
     {
-        initializeCyclic(0, 500, TimeUnit.MILLISECONDS, CycleBehavior.BestEffort);
+        initializeCyclic(0, 250, TimeUnit.MILLISECONDS, CycleBehavior.BestEffort);
         lbr = getContext().getDeviceFromType(LBR.class);
 
         // Start server listener in a separate thread
